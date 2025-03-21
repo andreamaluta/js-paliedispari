89af62603e1900cfe2c97206cb686c28e108a171
andreamaluta <andreamaluta@gmail.com>
2025-03-21 14:38:18 +0100

    [UPD] created the sum function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 function randomComputer(){
     let num = Math.floor(Math.random()*5 + 1)
-    console.log(num);
+    return num;
+}
+
+function sum(a,b){
+    return a+b;
 }
 
 let userChoice = prompt("Scegli se vuoi pari o dispari");
 console.log(userChoice);
 let userNum = parseInt(prompt("Scegli un numero da 1 a 5"));
 console.log(userNum);
+
+let computerNum = randomComputer();
+console.log(computerNum);
+
+let somma = sum(userNum, computerNum);
+console.log(somma);
