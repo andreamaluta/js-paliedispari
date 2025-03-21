567b7db243e4fab7e851da78124cc65d34657c67
andreamaluta <andreamaluta@gmail.com>
2025-03-21 14:34:01 +0100

    [UPD] made the prompt for the user

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function randomComputer(){
+    let num = Math.floor(Math.random()*5 + 1)
+    console.log(num);
+}
+
+let userChoice = prompt("Scegli se vuoi pari o dispari");
+console.log(userChoice);
+let userNum = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log(userNum);
