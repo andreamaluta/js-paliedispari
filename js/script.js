0a0c9d51ac966090f99d82f99f1ae757a3d007ea
andreamaluta <andreamaluta@gmail.com>
2025-03-21 15:07:37 +0100

    [UPD] commented the odd and even exercise

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
+// esercizio del pari e dispari
+// genero un numero random compreso tra 1 e 5 per il computer
 function randomComputer(){
     let num = Math.floor(Math.random()*5 + 1)
     return num;
 }
 
+// funzione per fare la somma
 function sum(a,b){
     return a+b;
 }
 
+// funzione booleana per controllare se è pari o dispari
 function isEven(a){
     if(a % 2 == 0){
         return true;
@@ -15,6 +19,7 @@ function isEven(a){
     }
 }
 
+// funzione che mi permette di decidere se ha vinto il computer o l'utente
 function winner(choice, even){
     if(even && choice=="pari"){
         console.log("l'utente ha vinto");
@@ -25,15 +30,19 @@ function winner(choice, even){
     }
 }
 
+// faccio scegliere all'utente se vuole pari o dispari
 let userChoice = prompt("Scegli se vuoi pari o dispari");
 console.log(userChoice);
+// faccio scegliere all'utente un numero da 1 a 5
 let userNum = parseInt(prompt("Scegli un numero da 1 a 5"));
 console.log(userNum);
-
+// uso la funzione per generare random un numero per il computer
 let computerNum = randomComputer();
 console.log(computerNum);
 
+// uso la funzione per calcolare la somma
 let somma = sum(userNum, computerNum);
 console.log(somma);
 
+// richiamo la funzione per decidere chi ha vinto
 winner(userChoice, isEven(somma));
