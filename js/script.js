ac666a130b5595d77b8982e675ba71491a99534f
andreamaluta <andreamaluta@gmail.com>
2025-03-21 15:04:10 +0100

    [UPD] completed the odd and even exercise

--- js/script.js
+++ js/script.js
@@ -7,6 +7,24 @@ function sum(a,b){
     return a+b;
 }
 
+function isEven(a){
+    if(a % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function winner(choice, even){
+    if(even && choice=="pari"){
+        console.log("l'utente ha vinto");
+    }else if(!even && choice=="dispari"){
+        console.log("l'utente ha vinto");
+    }else{
+        console.log("Il computer ha vinto");
+    }
+}
+
 let userChoice = prompt("Scegli se vuoi pari o dispari");
 console.log(userChoice);
 let userNum = parseInt(prompt("Scegli un numero da 1 a 5"));
@@ -17,3 +35,5 @@ console.log(computerNum);
 
 let somma = sum(userNum, computerNum);
 console.log(somma);
+
+winner(userChoice, isEven(somma));
